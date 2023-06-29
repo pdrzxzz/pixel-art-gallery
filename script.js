0b93097881043b0a98bfe105bf342db5a2a1b913
Emanuel Pedroza <109871758+pdrzxzz@users.noreply.github.com>
2023-06-29 15:42:35 -0300

    Create dark-icon path on js

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ function darkmode() {
     element = document.getElementsByClassName("img-description-wrapper")[i]
     element.classList.toggle("dark")
     }
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'dark-icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
 }
