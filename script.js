d85e954625cf1f8ef273f4bea0fd2118442741ac
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2023-06-29 20:11:52 -0300

    delete img-description and update media queries

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function darkmode() {
     let element = document.body;
     element.classList.toggle("dark");
-    for (i = 0; i < document.getElementsByClassName("img-description-wrapper").length; i++) {
-    let wrapperelement = document.getElementsByClassName("img-description-wrapper")[i]
-    wrapperelement.classList.toggle("dark")
-    }
     let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'icon';
