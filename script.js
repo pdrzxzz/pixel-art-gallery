f102af8fcb44bd60d051a5b1b648810d2b070e33
Emanuel Pedroza <109871758+pdrzxzz@users.noreply.github.com>
2023-06-29 16:21:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function darkmode() {
     let element = document.body;
     element.classList.toggle("dark");
     for (i = 0; i < document.getElementsByClassName("img-description-wrapper").length; i++) {
-    wrapperelement = document.getElementsByClassName("img-description-wrapper")[i]
+    let wrapperelement = document.getElementsByClassName("img-description-wrapper")[i]
     wrapperelement.classList.toggle("dark")
     }
     let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
